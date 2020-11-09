bff8bdfdd1b9579bf4cdb4892da77c2531c29cc5
akash-vhotkar <akashvhotkar4@gmail.com>
2020-11-09 15:30:59 +0530

    homepage is ready

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const session = require('ejs');
+const path = require('path');
+const body_parser = require('body-parser');
+
+
+//midelwares
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+
+//router setup
+app.get("/homepage", (req, res) => {
+    res.render('homepage');
+})
+
+
+// router part
+let port = process.env.PORT || 4000;
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    else console.log("server was running on the port " + port);
+})
